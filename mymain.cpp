034a8547d682e373d984376dac7c34e26ea1d7b0
Jared Wasserman <jaredbwasserman@gmail.com>
2025-11-22 17:30:30 -0800

    Update to final optimization for line drawing

--- mymain.cpp
+++ mymain.cpp
@@ -10,6 +10,38 @@ constexpr TGAColor red = {0, 0, 255, 255};
 constexpr TGAColor blue = {255, 128, 64, 255};
 constexpr TGAColor yellow = {0, 200, 255, 255};
 
+void line(int ax, int ay, int bx, int by, TGAImage& framebuffer, TGAColor color)
+{
+	bool steep = std::abs(ax - bx) < std::abs(ay - by);
+	if (steep)
+	{
+		// if the line is steep, we transpose the image
+		std::swap(ax, ay);
+		std::swap(bx, by);
+	}
+	if (ax > bx)
+	{
+		// make it left−to−right
+		std::swap(ax, bx);
+		std::swap(ay, by);
+	}
+	int y = ay;
+	int ierror = 0;
+	for (int x = ax; x <= bx; x++)
+	{
+		if (steep) // if transposed, de−transpose
+			framebuffer.set(y, x, color);
+		else
+			framebuffer.set(x, y, color);
+		ierror += 2 * std::abs(by - ay);
+		if (ierror > bx - ax)
+		{
+			y += by > ay ? 1 : -1;
+			ierror -= 2 * (bx - ax);
+		}
+	}
+}
+
 int main(int argc, char** argv)
 {
 	constexpr int width = 64;
@@ -19,6 +51,11 @@ int main(int argc, char** argv)
 	int ax = 7, ay = 3;
 	int bx = 12, by = 37;
 	int cx = 62, cy = 53;
+
+	line(ax, ay, bx, by, framebuffer, blue);
+	line(cx, cy, bx, by, framebuffer, green);
+	line(cx, cy, ax, ay, framebuffer, yellow);
+	line(ax, ay, cx, cy, framebuffer, red);
 
 	framebuffer.set(ax, ay, white);
 	framebuffer.set(bx, by, white);
