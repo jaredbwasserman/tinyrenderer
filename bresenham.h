d37391dd7d66515af6444dba0a41154d7cbd0d8c
Jared Wasserman <jaredbwasserman@gmail.com>
2025-11-22 18:47:32 -0800

    Fix main

---
+++ bresenham.h
@@ -0,0 +1,10 @@
+//
+// Created by Jared Wasserman on 11/22/25.
+//
+
+#ifndef TINYRENDERER_BRESENHAM_H
+#define TINYRENDERER_BRESENHAM_H
+
+int bresenham(int argc, char** argv);
+
+#endif //TINYRENDERER_BRESENHAM_H
--- mymain.cpp
+++ mymain.cpp
@@ -2,65 +2,9 @@
 // Created by Jared Wasserman on 11/22/25.
 //
 
-#include "tgaimage.h"
-
-constexpr TGAColor white = {255, 255, 255, 255}; // attention, BGRA order
-constexpr TGAColor green = {0, 255, 0, 255};
-constexpr TGAColor red = {0, 0, 255, 255};
-constexpr TGAColor blue = {255, 128, 64, 255};
-constexpr TGAColor yellow = {0, 200, 255, 255};
-
-void line(int ax, int ay, int bx, int by, TGAImage& framebuffer, TGAColor color)
-{
-	bool steep = std::abs(ax - bx) < std::abs(ay - by);
-	if (steep)
-	{
-		// if the line is steep, we transpose the image
-		std::swap(ax, ay);
-		std::swap(bx, by);
-	}
-	if (ax > bx)
-	{
-		// make it left−to−right
-		std::swap(ax, bx);
-		std::swap(ay, by);
-	}
-	int y = ay;
-	int ierror = 0;
-	for (int x = ax; x <= bx; x++)
-	{
-		if (steep) // if transposed, de−transpose
-			framebuffer.set(y, x, color);
-		else
-			framebuffer.set(x, y, color);
-		ierror += 2 * std::abs(by - ay);
-		if (ierror > bx - ax)
-		{
-			y += by > ay ? 1 : -1;
-			ierror -= 2 * (bx - ax);
-		}
-	}
-}
+#include "bresenham.h"
 
 int main(int argc, char** argv)
 {
-	constexpr int width = 64;
-	constexpr int height = 64;
-	TGAImage framebuffer(width, height, TGAImage::RGB);
-
-	int ax = 7, ay = 3;
-	int bx = 12, by = 37;
-	int cx = 62, cy = 53;
-
-	line(ax, ay, bx, by, framebuffer, blue);
-	line(cx, cy, bx, by, framebuffer, green);
-	line(cx, cy, ax, ay, framebuffer, yellow);
-	line(ax, ay, cx, cy, framebuffer, red);
-
-	framebuffer.set(ax, ay, white);
-	framebuffer.set(bx, by, white);
-	framebuffer.set(cx, cy, white);
-
-	framebuffer.write_tga_file("framebuffer.tga");
-	return 0;
+	return bresenham(argc, argv);
 }
