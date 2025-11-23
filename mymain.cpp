67fa204637d1aaa0d5922a2cd4411fee324fcd82
Jared Wasserman <jaredbwasserman@gmail.com>
2025-11-22 23:47:10 -0800

    Fix error output

--- mymain.cpp
+++ mymain.cpp
@@ -4,34 +4,47 @@
 
 #include <algorithm>
 #include <iostream>
+#include <ranges>
 #include <string>
 
 #include "bresenham.h"
 
+static const std::unordered_map<std::string, std::function<int()>> FUNCTIONS = {
+	{"bresenham", bresenham}
+};
+
+static void PrintFunctions()
+{
+	std::cout << "Select one of these functions:" << std::endl;
+	for (const auto& key : FUNCTIONS | std::views::keys)
+	{
+		std::cout << "\t" << key << std::endl;
+	}
+}
+
 int main(const int argc, char** argv)
 {
-	static const std::unordered_map<std::string, std::function<int()>> FUNCTIONS = {
-		{"bresenham", bresenham}
-	};
-
 	if (argc != 2)
 	{
-		std::cout << "Must provide exactly 1 argument." << std::endl;
+		std::cout << "Must provide a function." << std::endl;
+		PrintFunctions();
 		return 1;
 	}
 
 	std::string funcName{argv[1]};
 	std::ranges::transform(funcName, funcName.begin(), ::tolower);
 
-	for (const auto& pair : FUNCTIONS)
+	for (const auto& [key, val] : FUNCTIONS)
 	{
-		if (pair.first == funcName)
+		if (key == funcName)
 		{
 			std::cout << "Running function \"" << funcName << "\"." << std::endl;
-			return pair.second();
+			return val();
 		}
 	}
 
 	std::cout << "Invalid function \"" << funcName << "\"." << std::endl;
+	PrintFunctions();
+
 	return 1;
 }
